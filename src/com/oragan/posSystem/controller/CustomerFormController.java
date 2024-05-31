fdc7f9787158ed523bef5fe7d0255d71b8c48cb5
Pamindu Nawodya <pamidunawodya@gmail.com>
2024-05-31 16:54:29 +0530

    updated

---
+++ src/com/oragan/posSystem/controller/CustomerFormController.java
@@ -0,0 +1,10 @@
+/**
+ * authour By Pamindu Nawodya
+ * Date:5/31/2024
+ * Time:4:44 PM
+ * Project Name:oragan_pos_system
+ */
+package com.oragan.posSystem.controller;
+
+public class CustomerFormController {
+}
---
+++ src/com/oragan/posSystem/controller/ItemFormController.java
@@ -0,0 +1,10 @@
+/**
+ * authour By Pamindu Nawodya
+ * Date:5/31/2024
+ * Time:4:36 PM
+ * Project Name:oragan_pos_system
+ */
+package com.oragan.posSystem.controller;
+
+public class ItemFormController {
+}
