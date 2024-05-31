b2258a011d49e0ef42eb37701e1ac6c311ecface
Pamindu Nawodya <pamidunawodya@gmail.com>
2024-05-31 16:50:27 +0530

    update now set ui buttons locate views

--- src/com/oragan/posSystem/controller/DashBoardFormController.java
+++ src/com/oragan/posSystem/controller/DashBoardFormController.java
@@ -1,5 +1,12 @@
 package com.oragan.posSystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 /**
  * Created by Vishnuka Yahan
  *
@@ -8,4 +15,32 @@ package com.oragan.posSystem.controller;
  * @project : oragan_posSystem
  */
 public class DashBoardFormController {
+    public AnchorPane context;
+
+    private void setUi2(String ui2) throws IOException {
+        context.getChildren().clear();
+        Parent parent = FXMLLoader.load(getClass().getResource("../view/"+ui2+".fxml"));
+        context.getChildren().add(parent);
+    }
+
+
+    public void DashBoardOnAction(ActionEvent actionEvent) throws IOException {
+        setUi2("CustomerForm");
+    }
+
+    public void CustomerOnAction(ActionEvent actionEvent) throws IOException {
+        setUi2("ItemForm");
+    }
+
+    public void ItemOnAction(ActionEvent actionEvent) throws IOException {
+        setUi2("PurchaseOrderForm");
+    }
+
+    public void PurchaseOrderOnAction(ActionEvent actionEvent) throws IOException {
+        setUi2("OrderDetailsForm");
+    }
+
+    public void OderDetailsOnAction(ActionEvent actionEvent) throws IOException {
+        setUi2("DashBoardFunctionForm");
+    }
 }
