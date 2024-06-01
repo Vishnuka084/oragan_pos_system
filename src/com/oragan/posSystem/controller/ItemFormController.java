e0454d9ba1888777438a583b48c4ec49a7d230db
Pamindu Nawodya <pamidunawodya@gmail.com>
2024-06-01 22:25:36 +0530

    add Item Form Created

--- src/com/oragan/posSystem/controller/ItemFormController.java
+++ src/com/oragan/posSystem/controller/ItemFormController.java
@@ -1,5 +1,28 @@
 
 package com.oragan.posSystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class ItemFormController {
+    public void btnAddItemFormOnAction(ActionEvent actionEvent) throws IOException {
+        URL resource = this.getClass().getResource("/com/oragan/posSystem/view/AddItemForm.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(resource);
+        Parent load = fxmlLoader.load();
+        // AddCustomerFormController AddCustomerFormController = fxmlLoader.getController();
+        // AddCustomerFormController.init(tblStudent,this);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(load));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Add Customer Form");
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
