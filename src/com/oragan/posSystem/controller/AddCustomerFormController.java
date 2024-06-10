de61d746df9e9205027cfa0ad4f9e7393daef4ef
Pamindu Nawodya <pamidunawodya@gmail.com>
2024-06-10 16:51:33 +0530

    entity,dto updated

--- src/com/oragan/posSystem/controller/AddCustomerFormController.java
+++ src/com/oragan/posSystem/controller/AddCustomerFormController.java
@@ -6,5 +6,29 @@
  */
 package com.oragan.posSystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class AddCustomerFormController {
+
+
+    public AnchorPane addCustomerFormContext;
+    public TextField txtCustomerId;
+    public TextField txtCustomerName;
+    public TextField txtCustomerAddress;
+    public TextField txtContactNumber;
+
+    public void btnClearTxtFldOnAction(ActionEvent actionEvent) {
+        txtCustomerId.clear();
+        txtCustomerName.clear();
+        txtCustomerAddress.clear();
+        txtContactNumber.clear();
+    }
+
+    public void btnCustomerAddOnAction(ActionEvent actionEvent) {
+        String customerID=txtCustomerId.getText();
+
+
+    }
 }
