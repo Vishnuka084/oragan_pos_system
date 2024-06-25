a1326d12b23bc6b747f2a5bc956487690d319fbf
Pamindu Nawodya <pamidunawodya@gmail.com>
2024-06-25 11:45:19 +0530

    updated

---
+++ src/com/oragan/posSystem/entity/Order.java
@@ -0,0 +1,72 @@
+/**
+ * authour By Pamindu Nawodya
+ * Date:6/24/2024
+ * Time:10:18 PM
+ * Project Name:oragan_pos_system
+ */
+package com.oragan.posSystem.entity;
+
+import java.sql.Date;
+
+public class Order {
+
+    private String OrderID;
+    private Date OrderDate;
+    private double Total;
+    private String Customer_Id;
+
+
+    public Order(String orderID) {
+        OrderID = orderID;
+    }
+
+    public Order(String orderID, Date orderDate, double total, String customer_Id) {
+        OrderID = orderID;
+        OrderDate = orderDate;
+        Total = total;
+        Customer_Id = customer_Id;
+    }
+
+
+    public String getOrderID() {
+        return OrderID;
+    }
+
+    public void setOrderID(String orderID) {
+        OrderID = orderID;
+    }
+
+    public Date getOrderDate() {
+        return OrderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        OrderDate = orderDate;
+    }
+
+    public double getTotal() {
+        return Total;
+    }
+
+    public void setTotal(double total) {
+        Total = total;
+    }
+
+    public String getCustomer_Id() {
+        return Customer_Id;
+    }
+
+    public void setCustomer_Id(String customer_Id) {
+        Customer_Id = customer_Id;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "OrderID='" + OrderID + '\'' +
+                ", OrderDate=" + OrderDate +
+                ", Total=" + Total +
+                ", Customer_Id='" + Customer_Id + '\'' +
+                '}';
+    }
+}
---
+++ src/com/oragan/posSystem/entity/OrderItem.java
@@ -0,0 +1,90 @@
+/**
+ * authour By Pamindu Nawodya
+ * Date:6/24/2024
+ * Time:7:02 PM
+ * Project Name:oragan_pos_system
+ */
+package com.oragan.posSystem.entity;
+
+public class OrderItem {
+
+
+    private String OrderID;
+    private String Item_code;
+    private  String Item_name;
+    private Integer Quantity;
+    private double price;
+    private double total;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(String orderID, String item_code, String item_name, Integer quantity, double price, double total) {
+        OrderID = orderID;
+        Item_code = item_code;
+        Item_name = item_name;
+        Quantity = quantity;
+        this.price = price;
+        this.total = total;
+    }
+
+    public String getOrderID() {
+        return OrderID;
+    }
+
+    public void setOrderID(String orderID) {
+        OrderID = orderID;
+    }
+
+    public String getItem_code() {
+        return Item_code;
+    }
+
+    public void setItem_code(String item_code) {
+        Item_code = item_code;
+    }
+
+    public String getItem_name() {
+        return Item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        Item_name = item_name;
+    }
+
+    public Integer getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        Quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "OrderID='" + OrderID + '\'' +
+                ", Item_code='" + Item_code + '\'' +
+                ", Item_name='" + Item_name + '\'' +
+                ", Quantity=" + Quantity +
+                ", price=" + price +
+                ", total=" + total +
+                '}';
+    }
+}
